b0dd465edb9682eb89fb8875ddad346d326e1850
Fujimura Daisuke <dfujimura@aiming-inc.com>
2012-03-21 14:39:11 +0900

    modularize

--- lib/nippou.rb
+++ lib/nippou.rb
@@ -4,26 +4,32 @@ require 'pit'
 require 'mail'
 require 'core_ext/editable'
 require "nippou/version"
-config = Pit.get("nippou", :require => {
-  "name"       => "Your name",
-  "email"      => "Your email",
-  "password"   => "Password of your email",
-  "recipients" => "Recipients"
-})
+module Nippou
+  class Base
+    DEFAULT_CHARSET = 'utf-8'
+    def self.deliver
+      config = Pit.get("nippou", :require => {
+                         "name"       => "Your name",
+                         "email"      => "Your email",
+                         "password"   => "Password of your email",
+                         "recipients" => "Recipients"})
 
-mail         = Mail.new
-mail.to      = config['recipients']
-mail.from    = config['email']
-mail.charset = 'UTF-8'
-mail.body    = (config['body'] || 'write your nippou here').dup.edit! # OMG
-mail.delivery_method :smtp, {
-  :address              => 'smtp.gmail.com',
-  :port                 => 587,
-  :domain               => (config['domain'] || 'smtp.gmail.com'),
-  :user_name            => config['email'],
-  :password             => config['password'],
-  :authentication       => :plain,
-  :enable_starttls_auto => true
-}
+      mail         = Mail.new
+      mail.to      = config['recipients']
+      mail.from    = config['email']
+      mail.charset = DEFAULT_CHARSET
+      mail.body    = (config['body'] || 'write your nippou here').dup.edit! # OMG
+      mail.delivery_method :smtp, {
+        :address              => 'smtp.gmail.com',
+        :port                 => 587,
+        :domain               => (config['domain'] || 'smtp.gmail.com'),
+        :user_name            => config['email'],
+        :password             => config['password'],
+        :authentication       => :plain,
+        :enable_starttls_auto => true
+      }
 
-mail.deliver
+      mail.deliver
+    end
+  end
+end
